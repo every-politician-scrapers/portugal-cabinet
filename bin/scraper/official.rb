c82f418a368269122ba652800192a16c111d586c
Tony Bowden <tony@tmtm.com>
2021-11-10 14:12:33 +0200

    [Cupdate to EPS 0.1.13

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -5,13 +5,12 @@ require 'every_politician_scraper/scraper_data'
 require 'pry'
 
 class MemberList
-  # details for an individual member
-  class Member < Scraped::HTML
-    field :name do
+  class Member
+    def name
       noko.css('.name').text.tidy
     end
 
-    field :position do
+    def position
       noko.css('.title').text.tidy
     end
 
@@ -20,17 +19,7 @@ class MemberList
     end
   end
 
-  # The page listing all the members
-  class Members < Scraped::HTML
-    field :members do
-      member_container.flat_map do |member|
-        data = fragment(member => Member).to_h
-        [data.delete(:position)].flatten.map { |posn| data.merge(position: posn) }
-      end
-    end
-
-    private
-
+  class Members
     def member_container
       noko.css('div.item')
     end
