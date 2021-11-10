85f25d8cd64398f437b0aad63a67f2d0e7f36ac2
Tony Bowden <tony@tmtm.com>
2021-11-10 14:20:02 +0200

    Don't report on junior ministers
    It would be good to include these at some point, but for now we just
    make sure the seniors are correct.

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,16 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+# Process the data from each source before comparison
+class Comparison < EveryPoliticianScraper::Comparison
+  def external
+    super.delete_if { |row| row[:position].to_s =~ /(secretary of state)|(deputy minister)/i }
+  end
+
+  def wikidata
+    super.delete_if { |row| row[:position].to_s =~ /(secretary of state)|(deputy minister)/i }
+  end
+end
+
+diff = Comparison.new('data/wikidata.csv', 'data/official.csv').diff
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
