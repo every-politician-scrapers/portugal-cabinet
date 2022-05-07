a6863bc42758334478babb4c21ca9dce525e8deb
Tony Bowden <tony@tmtm.com>
2022-05-07 08:00:00 +0300

    don't compare start dates

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -5,6 +5,12 @@ require 'every_politician_scraper/comparison'
 
 # Process the data from each source before comparison
 class Comparison < EveryPoliticianScraper::NulllessComparison
+  # Start date on official site is within the current Cabinet,
+  # whereas Wikidata can span multiple Cabinets, so don't compare those
+  def columns
+    super - %i[start]
+  end
+
   def external
     super.delete_if { |row| row[:position].to_s =~ /(secretary of state)|(deputy minister)/i }
   end
