041c8dfcff36ad7a3a309133243e34bffaeaecb6
Tony Bowden <tony@tmtm.com>
2022-05-07 07:56:35 +0300

    bring scraper up-to-date

--- sources/central-bank-enwiki/scraper.rb
+++ sources/central-bank-enwiki/scraper.rb
@@ -1,111 +1,60 @@
 #!/bin/env ruby
 # frozen_string_literal: true
 
-require 'csv'
+require 'every_politician_scraper/scraper_data'
 require 'pry'
-require 'scraped'
-require 'table_unspanner'
-require 'wikidata_ids_decorator'
 
-require 'open-uri/cached'
-
-class WikiDate
-  REMAP = {
-    'Presente' => '',
-  }.freeze
-
-  def initialize(date_str)
-    @date_str = date_str
+class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
+  def empty?
+    too_early?
   end
 
-  def to_s
-    return if date_en.to_s.empty?
-    return date_en if date_en =~ /^\d{4}$/
-
-    raise "Unknown date format: #{date_en}"
+  def combo_date?
+    true
   end
 
-  private
-
-  attr_reader :date_str
-
-  def date_obj
-    @date_obj ||= Date.parse(date_en)
+  def raw_combo_date
+    raise 'need to define a raw_combo_date'
   end
 
-  def date_en
-    @date_en ||= REMAP.reduce(date_str) { |str, (ro, en)| str.sub(ro, en) }
+  def name_node
+    raise 'need to define a name_node'
+  end
+
+  def too_early?
+    start_year < ignore_before
   end
 end
 
-class RemoveReferences < Scraped::Response::Decorator
-  def body
-    Nokogiri::HTML(super).tap do |doc|
-      doc.css('sup.reference').remove
-    end.to_s
-  end
-end
 
-class UnspanAllTables < Scraped::Response::Decorator
-  def body
-    Nokogiri::HTML(super).tap do |doc|
-      doc.css('table.wikitable').each do |table|
-        unspanned_table = TableUnspanner::UnspannedTable.new(table)
-        table.children = unspanned_table.nokogiri_node.children
-      end
-    end.to_s
-  end
-end
-
-class MinistersList < Scraped::HTML
+class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
-  decorator UnspanAllTables
+  # decorator UnspanAllTables
   decorator WikidataIdsDecorator::Links
 
-  field :ministers do
-    member_entries.map { |ul| fragment(ul => Officeholder) }.reject(&:empty?).map(&:to_h).uniq
+  # TODO: make this easier to override
+  def holder_entries
+    noko.xpath("//h3[.//span[contains(.,'precedentes')]]//following-sibling::ol[1]//li")
   end
 
-  private
+  class Officeholder < OfficeholderNonTable
+    def raw_combo_date
+      noko.text.scan(/\((.*?)\)/).flatten.last.gsub('Presente', 'Incumbent')
+    end
 
-  def member_entries
-    noko.xpath('//h3[contains(.,"Governadores precedentes")]/following-sibling::ol[1]//li')
-  end
-end
+    def itemLabel
+      noko.css('a').map(&:text).last
+    end
 
-class Officeholder < Scraped::HTML
-  def empty?
-    itemLabel.include? 'Vacant'
-  end
+    def item
+      noko.css('a/@wikidata').map(&:text).last
+    end
 
-  field :item do
-    noko.css('a/@wikidata').map(&:text).first
-  end
-
-  field :itemLabel do
-    noko.css('a').map(&:text).first
-  end
-
-  field :startDate do
-    WikiDate.new(dates[0]).to_s
-  end
-
-  field :endDate do
-    WikiDate.new(dates[1]).to_s
-  end
-
-  private
-
-  def dates
-    noko.text.scan(/\((.*?)\)/).flatten.last.split('-').map(&:tidy)
+    def ignore_before
+      1991
+    end
   end
 end
 
 url = ARGV.first
-data = MinistersList.new(response: Scraped::Request.new(url: url).response).ministers
-
-header = data.first.keys.to_csv
-rows = data.map { |row| row.values.to_csv }
-abort 'No results' if rows.count.zero?
-
-puts header + rows.join
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
