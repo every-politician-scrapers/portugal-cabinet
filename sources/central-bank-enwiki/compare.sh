ce57709e61483886ca24d40bb36639a959796c6d
Tony Bowden <tony@tmtm.com>
2022-05-28 08:24:12 +0300

    quieten compare.sh cleanup

--- sources/central-bank-enwiki/compare.sh
+++ sources/central-bank-enwiki/compare.sh
@@ -8,6 +8,6 @@ bundle exec ruby scraper.rb $(jq -r .source meta.json) > scraped.csv
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s startDate > wikidata.csv
 bundle exec ruby diff.rb | qsv sort -s startdate | tee diff.csv
 
-git clean -fX
+git clean -qfX
 
 cd ~-
